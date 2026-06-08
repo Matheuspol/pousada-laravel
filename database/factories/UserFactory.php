3f6c09f8369fe9582892b327b34bf5724fe95efd
Matheus Pol Troglio <204792268+Matheuspol@users.noreply.github.com>
2026-06-08 10:49:46 -0300

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
+
 class UserFactory extends Factory
 {
+
     protected static ?string $password;
 
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    
     public function definition(): array
     {
         return [
