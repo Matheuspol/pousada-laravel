0df1b33e6e58879770c18f03bbc8f3e05d221571
Matheus Pol Troglio <204792268+Matheuspol@users.noreply.github.com>
2026-06-08 10:23:14 -0300

    Add files via upload
    Upload anexo documentos PDF para hospedes

---
+++ Hospede.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Hospede extends Model
+{
+    use HasFactory;
+
+    protected $table = 'hospedes';
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'email',
+        'telefone',
+        'cidade',
+        'estado',
+        'anexo',
+    ];
+
+    // Um hóspede pode ter muitas reservas.
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class, 'hospede_id');
+    }
+}
